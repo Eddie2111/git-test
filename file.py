b93fba84b23d61b8e85c64a870f2b9f4c0903be2
A S M TAREQ MAHMOOD <20101073@bracu.ac.bd>
2023-06-22 15:16:52 +0600

    feat(init): file changed

---
+++ file.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self):
+        pass
+    
+    def UI(self,button: str)-> str:
+        self.button = button
+        pass
+    def Back(self, auth: bool) -> bool:
+        self.auth = auth
+
+node1 = Node()
+node2 = Node()
+node3 = Node()
